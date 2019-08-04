de9a7848764bfd7400724996ada65b052ae45783
Advay Mengle (at Holvonix LLC) <advay-github@holvonix.com>
2019-08-03 17:20:17 -0700

    test: improve registration tests

--- test/test-registry.ts
+++ test/test-registry.ts
@@ -6,6 +6,37 @@ import { Fuzzer } from '../src';
 import * as t from 'io-ts';
 
 describe('registry', () => {
+  const fuzzStr1: Fuzzer<string, t.StringType> = {
+    id: 'StringType',
+    idType: 'tag',
+    impl: {
+      type: 'fuzzer',
+      func: n => {
+        return `Hello ${n}`;
+      },
+    },
+  };
+  const fuzzNum: Fuzzer<number, t.NumberType> = {
+    id: 'NumberType',
+    idType: 'tag',
+    impl: {
+      type: 'fuzzer',
+      func: n => {
+        return n + 1;
+      },
+    },
+  };
+  const fuzzStr2: Fuzzer<string, t.StringType> = {
+    id: 'StringType',
+    idType: 'tag',
+    impl: {
+      type: 'fuzzer',
+      func: n => {
+        return `Bye ${n}`;
+      },
+    },
+  };
+
   describe('#createRegistry', () => {
     describe('#getFuzzer', () => {
       it(`has no fuzzers`, () => {
@@ -18,13 +49,55 @@ describe('registry', () => {
       });
     });
 
-    describe('#exampleGenerator', () => {
-      for (const b of types) {
-        it(`can create an example generator for \`${b.name}\` type`, () => {
-          const r = lib.createCoreRegistry().exampleGenerator(b);
-          assert.ok(r);
-        });
-      }
+    describe('#register', () => {
+      it('registers a simple fuzzer', () => {
+        const r = lib.createRegistry();
+        assert.strictEqual(r.register(fuzzStr1), r);
+        assert.strictEqual(r.getFuzzer(t.string), fuzzStr1);
+      });
+
+      it('registers multiple distinct fuzzers sequentially', () => {
+        const r = lib.createRegistry();
+        assert.strictEqual(r.register(fuzzStr1), r);
+        assert.strictEqual(r.register(fuzzNum), r);
+        assert.strictEqual(r.getFuzzer(t.string), fuzzStr1);
+        assert.strictEqual(r.getFuzzer(t.number), fuzzNum);
+      });
+
+      it('registers multiple distinct fuzzers bulk', () => {
+        const r = lib.createRegistry();
+        assert.strictEqual(r.register(...([fuzzStr1, fuzzNum] as Fuzzer[])), r);
+        assert.strictEqual(r.getFuzzer(t.string), fuzzStr1);
+        assert.strictEqual(r.getFuzzer(t.number), fuzzNum);
+      });
+
+      it('overrides 1st registration for 1 type with the 2nd across register calls', () => {
+        let r = lib.createRegistry();
+        assert.strictEqual(r.register(fuzzStr1), r);
+        assert.strictEqual(r.register(fuzzStr2), r);
+        assert.strictEqual(r.getFuzzer(t.string), fuzzStr2);
+
+        r = lib.createRegistry();
+        assert.strictEqual(r.register(fuzzStr2), r);
+        assert.strictEqual(r.register(fuzzStr1), r);
+        assert.strictEqual(r.getFuzzer(t.string), fuzzStr1);
+      });
+
+      it('overrides 1st registration for 1 type with the 2nd in one register call', () => {
+        let r = lib.createRegistry();
+        assert.strictEqual(
+          r.register(fuzzStr1 as Fuzzer, fuzzStr2 as Fuzzer),
+          r
+        );
+        assert.strictEqual(r.getFuzzer(t.string), fuzzStr2);
+
+        r = lib.createRegistry();
+        assert.strictEqual(
+          r.register(fuzzStr2 as Fuzzer, fuzzStr1 as Fuzzer),
+          r
+        );
+        assert.strictEqual(r.getFuzzer(t.string), fuzzStr1);
+      });
     });
   });
 
